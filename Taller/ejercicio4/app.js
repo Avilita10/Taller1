b02d635e43f3afc208fab44a8cc893dbf84fa737
Avilita10 <141172974+Avilita10@users.noreply.github.com>
2023-08-08 15:30:19 -0500

    app.js

--- Taller/ejercicio4/app.js
+++ Taller/ejercicio4/app.js
@@ -6,15 +6,13 @@ let number2 = parseFloat(prompt('Ingrese el segundo digito: '));
 
 let number3 = parseFloat(prompt('Ingrese el tercer digito: '));
 
-if(number1 === number2 === number3){
+if(number1 === number2 && number2 === number3){
     alert('Todos los numeros son iguales');
-}else if(number1 > number2 > number3){
+}else if(number1 > number2 && number2 > number3){
     alert('El numero mayor es: '+number1);
-}else if(number1 < number2 > number3){
+}else if(number1 < number2 && number2 > number3){
     alert('El numero mayor es: '+number2);
-}else if(number1 === number2 === number3){
-    alert('Todos los numeros son iguales');
-}else if(number1 < number2 < number3){
+}else if(number1 < number2 && number2 < number3){
     alert('El numero mayor es: '+number3);
 }
 
